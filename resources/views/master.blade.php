0f7b19a9914ec3e4ae8dda917c35b60c2c34cd51
Hiren <hirenkeradiya@gmail.com>
2023-09-25 00:56:47 +0530

    Update assets

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -37,11 +37,11 @@
 
 	<script src="{{ asset('assets/front/js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('assets/front/js/jquery.min.js') }}"></script>
-    <script src="{{ asset('assets/front/plugins/simplebar/js/simplebar.min.js') }}"></script>
-    <script src="{{ asset('assets/front/plugins/OwlCarousel/js/owl.carousel.min.js') }}"></script>
-    <script src="{{ asset('assets/front/plugins/OwlCarousel/js/owl.carousel2.thumbs.min.js') }}"></script>
-    <script src="{{ asset('assets/front/plugins/metismenu/js/metisMenu.min.js') }}"></script>
-    <script src="{{ asset('assets/front/plugins/perfect-scrollbar/js/perfect-scrollbar.js') }}"></script>
+    <script src="{{ asset('assets/front/js/simplebar.min.js') }}"></script>
+    <script src="{{ asset('assets/front/js/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('assets/front/js/owl.carousel2.thumbs.min.js') }}"></script>
+    <script src="{{ asset('assets/front/js/metisMenu.min.js') }}"></script>
+    <script src="{{ asset('assets/front/js/perfect-scrollbar.js') }}"></script>
     <script src="{{ asset('assets/front/js/app.js') }}"></script>
     <script src="{{ asset('assets/front/js/index.js') }}"></script>
 
